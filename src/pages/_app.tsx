b01d74d723d12401fb45e744ff178360c99bb65d
CarlesRojas <carles.rojas@mundimoto.com>
2022-12-03 15:22:14 +0100

    create project model

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,6 @@ import { type AppType } from 'next/app';
 
 import { trpc } from '../utils/trpc';
 
-import '../styles/globals.css';
-
 const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,11 +1,6 @@
-import { router } from "../trpc";
-import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
+import { router } from '../trpc';
 
-export const appRouter = router({
-  example: exampleRouter,
-  auth: authRouter,
-});
+export const appRouter = router({});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
