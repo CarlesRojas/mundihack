7cf691aca46bc315f44e55901893b5a6d4b24e9c
CarlesRojas <carles.rojas@mundimoto.com>
2022-12-03 18:53:06 +0100

    config react query

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -18,15 +18,25 @@ export const trpc = createTRPCNext<AppRouter>({
       links: [
         loggerLink({
           enabled: (opts) =>
-            process.env.NODE_ENV === 'development' || (opts.direction === 'down' && opts.result instanceof Error)
+            process.env.NODE_ENV === 'development' || (opts.direction === 'down' && opts.result instanceof Error),
         }),
         httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`
-        })
-      ]
+          url: `${getBaseUrl()}/api/trpc`,
+        }),
+      ],
+      queryClientConfig: {
+        defaultOptions: {
+          queries: {
+            refetchOnMount: true,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+            refetchOnError: false,
+          },
+        },
+      },
     };
   },
-  ssr: false
+  ssr: true,
 });
 
 /**
