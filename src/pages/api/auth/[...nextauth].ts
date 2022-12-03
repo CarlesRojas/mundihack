b7d6dda482e03c1f4923506110b4b16f64d473d1
CarlesRojas <carles.rojas@mundimoto.com>
2022-12-03 14:35:47 +0100

    create t3 app

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,24 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+import { env } from '../../../env/server.mjs';
+import { prisma } from '../../../server/db/client';
+
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) session.user.id = user.id;
+      return session;
+    }
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET
+    })
+  ]
+};
+
+export default NextAuth(authOptions);
